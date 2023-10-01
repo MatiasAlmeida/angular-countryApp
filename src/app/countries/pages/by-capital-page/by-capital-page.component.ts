7b2ed4c934e55cb064741b7b4697db46f6fb77a5
Matias Almeida <matiasleonelalmeida97@gmail.com>
2023-10-01 10:59:17 -0300

    added component search-box communication with pages to make searchs

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-by-capital-page',
+  selector: 'countries-by-capital-page',
   templateUrl: './by-capital-page.component.html',
   styles: [
   ]
 })
 export class ByCapitalPageComponent {
 
+  public searchByCapital( term: string ): void {
+    console.log({ source: 'Desde ByCapitalPage' });
+    console.log({ term });
+  }
 }
