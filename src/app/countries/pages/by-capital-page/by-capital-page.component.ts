34388990ef6f1274cc6a675e71ed9fe389519beb
Matias Almeida <matiasleonelalmeida97@gmail.com>
2023-10-01 22:40:28 -0300

    added country table component

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'countries-by-capital-page',
@@ -7,9 +9,14 @@ import { Component, EventEmitter, Output } from '@angular/core';
   ]
 })
 export class ByCapitalPageComponent {
+  public countries: Country[] = [];
+
+  constructor( private countriesService: CountriesService ) { }
 
   public searchByCapital( term: string ): void {
-    console.log({ source: 'Desde ByCapitalPage' });
-    console.log({ term });
+    this.countriesService.searchCapital(term)
+      .subscribe( countries => {
+        this.countries = countries;
+      });
   }
 }
